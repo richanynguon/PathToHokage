203de791edc4cceda3d14e1d219e728ac8fed285
richanynguon <alina.nguon@gmail.com>
2020-07-14 11:51:13 -0400

    complete logs

--- CodeChallenge/bloomberg-prep/Monday/CompleteLogs.js
+++ CodeChallenge/bloomberg-prep/Monday/CompleteLogs.js
@@ -1,4 +1,9 @@
 /*
-Given incomplete logs froma race, find the order of the winners
+
+Given incomplete logs from race, find the order of the winners
 https://www.geeksforgeeks.org/puzzle-9-find-the-fastest-3-horses/
+
+
+if 25 horses, race 5 each get top 5 race those get top 3
+
 */

0dacb9e5bf1dce38fdd354fb9edc625c3a6bc1f8
Richany <alina.nguon@gmail.com>
2020-11-17 13:13:00 -0500

    stacks with postfix procedure

--- C-CPP/Udemy_Data_Structures_Algorithms/stack.c
+++ C-CPP/Udemy_Data_Structures_Algorithms/stack.c
@@ -14,6 +14,9 @@ Operations:
 6. isFull
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 struct Stack
 {
     int size;
@@ -205,4 +208,434 @@ int main() {
  
     return 0;
 }
+*/
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*top=NULL;
+
+void pushLLStack(int x)
+{
+    struct Node *t;
+    t=(struct Node*)malloc(sizeof(struct Node));
+
+    if(t==NULL)
+        printf("stack is full\n");
+    else
+    {
+        t->data=x;
+        t->next=top;
+        top=t;
+    }
+
+}
+
+int popLLStack()
+{
+    struct Node *t;
+    int x=-1;
+
+    if(top==NULL)
+        printf("Stack is Empty\n");
+    else
+    {
+        t=top;
+        top=top->next;
+        x=t->data;
+        free(t);
+    }
+    return x;
+}
+
+void DisplayLLStack()
+{
+    struct Node *p;
+    p=top;
+    while(p!=NULL)
+    {
+        printf("%d ",p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+
+/*
+cpp
+#include <iostream>
+using namespace std;
+ 
+class Node{
+public:
+    int data;
+    Node* next;
+};
+ 
+class Stack{
+private:
+    Node* top;
+public:
+    Stack();
+    ~Stack();
+    void push(int x);
+    int pop();
+    int peek(int index);
+    int isEmpty();
+    int isFull();
+    int stackTop();
+};
+ 
+Stack::Stack() {
+    top = nullptr;
+}
+ 
+Stack::~Stack() {
+    Node* p = top;
+    while (top){
+        top = top->next;
+        delete p;
+        p = top;
+    }
+}
+ 
+void Stack::push(int x) {
+    Node* t = new Node;
+    if (t == nullptr){
+        cout << "Stack Overflow!" << endl;
+    } else {
+        t->data = x;
+        t->next = top;
+        top = t;
+    }
+}
+ 
+int Stack::pop() {
+    Node* p;
+    int x = -1;
+    if (top == nullptr){
+        cout << "Stack Underflow!" << endl;
+    } else {
+        p = top;
+        x = p->data;
+        top = top->next;
+        delete p;
+    }
+    return x;
+}
+ 
+int Stack::isFull() {
+    Node* t = new Node;
+    int r = t ? 1 : 0;
+    delete t;
+    return r;
+}
+ 
+int Stack::isEmpty() {
+    return top ? 0 : 1;
+}
+ 
+int Stack::stackTop() {
+    if (top){
+        return top->data;
+    }
+    return -1;
+}
+ 
+int Stack::peek(int index) {
+    if (isEmpty()){
+        return -1;
+    } else {
+        Node* p = top;
+ 
+        for (int i=0; p != nullptr && i<index-1; i++){
+            p = p->next;
+        }
+ 
+        if (p != nullptr){
+            return p->data;
+        } else {
+            return -1;
+        }
+    }
+}
+ 
+int main() {
+ 
+    int A[] = {1, 3, 5, 7, 9};
+ 
+    Stack stk;
+ 
+    // Populate stack
+    for (int i=0; i<sizeof(A)/sizeof(A[0]); i++){
+        stk.push(A[i]);
+    }
+ 
+    cout << "Stack peek at 3rd: " << stk.peek(3) << endl;
+    cout << "Stack peek at 10th: " << stk.peek(10) << endl;
+    cout << "Stack top: " << stk.stackTop() << endl;
+    cout << "Stack full: " << stk.isFull() << endl;
+    cout << "Stack empty: " << stk.isEmpty() << endl;
+ 
+    // Pop out elements from stack
+    cout << "Popped: " << flush;
+    for (int i=0; i<sizeof(A)/sizeof(A[0]); i++){
+        cout << stk.pop() << ", " << flush;
+    }
+    cout << endl;
+ 
+    // Underflow
+    cout << stk.pop() << endl;
+ 
+    return 0;
+}
+*/
+
+int isBalanced(char *exp)
+{
+    int i;
+
+    for(i=0;exp[i]!='\0';i++)
+    {
+        if(exp[i]=='(')
+            pushLLStack(exp[i]);
+        else if(exp[i]==')')
+        {
+            if(top==NULL)
+                return 0;
+            popLLStack();
+        }
+    }
+    if(top==NULL)
+        return 1;
+    else
+        return 0;
+}
+
+/*
+cpp
+#include <iostream>
+#include<cstring>
+#include <stack>
+#include <map>
+
+using namespace std;
+ 
+int isBalanced(char* exp){
+ 
+    // Create map
+    map<char, char> mapping;
+    mapping['}'] = '{';
+    mapping[')'] = '(';
+    mapping[']'] = '[';
+ 
+    // Create map iterator
+    map<char, char>::iterator itr;
+ 
+    // Create stack
+    stack<char> stk;
+ 
+    for (int i=0; i<strlen(exp); i++){
+        if (exp[i] == '{' || exp[i] == '[' || exp[i] == '('){
+            stk.push(exp[i]);
+        } else if (exp[i] == '}' || exp[i] == ']' || exp[i] == ')'){
+            if (stk.empty()){
+                return false;
+            } else {
+                char temp = stk.top();
+                itr = mapping.find(exp[i]);
+                if (temp == itr->second){  // itr->first is key, itr->second is value
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return stk.empty() ? true : false;
+}
+ 
+int main() {
+ 
+    char A[] = "{([a+b]*[c-d])/e}";
+    cout << isBalanced(A) << endl;
+ 
+    char B[] = "{([a+b]}*[c-d])/e}";
+    cout << isBalanced(B) << endl;
+ 
+    char C[] = "{([{a+b]*[c-d])/e}";
+    cout << isBalanced(C) << endl;
+ 
+    return 0;
+}
+*/
+
+/*
+Infix to Postfix Conversion
+
+1. What is postfix
+2. Why postfix
+3. Precedence
+4. Manual Conversion
+
+
+Infix: a+b
+Prefix: +ab
+Postfix: ab+
+
+infix
+a+b*c
+
+prefix
++a*bc
+
+postfix
+abc*+
+
+infix
+a+b+c*d
+
+prefix
+++ab*cd
+
+postfix
+ab+cd*+
+
+
+*+ab-cd
+ab+cd-*
+
+1. Associativity
+2. Unary Operators
+
+Sym |Pre|Assoc
++,- | 1 | L-R
+*,/ | 2 | L-R 
+^   | 3 | R-L
+∪,∩,| 4 | R-L (also can be - + *)
+()  | 5 | L-R
+
+
+
+*/
+
+
+#include<strings.h>
+
+int pre(char x)
+{
+ if(x=='+' || x=='-')
+ return 1;
+ else if(x=='*' || x=='/')
+ return 2;
+ return 0;
+}
+
+int isOperand(char x)
+{
+    if(x=='+' || x=='-' || x=='*' || x=='/')
+        return 0;
+    else
+        return 1;
+
+}
+
+char * InToPost(char *infix)
+{
+    int i=0,j=0;
+    char *postfix;
+    int len=strlen(infix);
+    postfix=(char *)malloc((len+2)*sizeof(char));
+
+    while(infix[i]!='\0')
+    {
+        if(isOperand(infix[i]))
+            postfix[j++]=infix[i++];
+        else
+        {
+            if(pre(infix[i])>pre(top->data))
+                pushLLStack(infix[i++]);
+            else
+            {
+                postfix[j++]=popLLStack();
+            }
+        }
+    }
+    while(top!=NULL)
+        postfix[j++]=popLLStack();
+    postfix[j]='\0';
+    return postfix;
+}
+
+
+char * InToPost2(char *infix)
+{
+    int i=0,j=0;
+    char *postfix;
+    long len=strlen(infix);
+    postfix=(char *)malloc((len+2)*sizeof(char));
+
+    while(infix[i]!='\0')
+    {
+        if(isOperand(infix[i]))
+            postfix[j++]=infix[i++];
+        else
+        {
+            if(pre(infix[i])>pre(top->data))
+                pushLLStack(infix[i++]);
+            else
+            {
+                postfix[j++]=popLLStack();
+            }
+        }
+    }
+    while(top!=NULL)
+        postfix[j++]=popLLStack();
+    postfix[j]='\0';
+    return postfix;
+}
+
+int Eval(char *postfix)
+{
+    int i=0;
+    int x1,x2,r=0 ;
+
+    for(i=0;postfix[i]!='\0';i++)
+    {
+        if(isOperand(postfix[i]))
+        {
+            pushLLStack(postfix[i]-'0');
+        }
+        else
+        {
+            x2=popLLStack();
+            x1=popLLStack();
+            switch(postfix[i])
+            {
+                case '+':r=x1+x2; break;
+                case '-':r=x1-x2; break;
+                case '*':r=x1*x2; break;
+                case '/':r=x1/x2; break;
+            }
+            pushLLStack(r);
+        }
+    }
+    return top->data;
+}
+
+/*
+OSP Out Stack Presendence
+ISP Inside Stack Presendence
+ 
+Check the OSP of each symbol to push in the stack
+Check the ISP to pop out of the stack
+If they are operands then send
+Sym|OSP|ISP
+-----------
++,-|1  |2
+*,/|3  |4
+^  |6  |5
+(  |7  |0
+)  |0  |?
+
 */
